dacc7e4037fffe5a7d11dd2cf8e74e055c8c60bf
adoringonion <adringonion.byazura@gmail.com>
2021-10-01 22:49:36 +0900

    google analytics

--- public/index.js
+++ public/index.js
@@ -3,6 +3,16 @@
 export default {
   load: async function (elmLoaded) {
     const app = await elmLoaded;
+
+    (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-147049916-2', 'auto');
+    ga('send', 'pageview');
     console.log("App loaded", app);
   },
   flags: function () {
