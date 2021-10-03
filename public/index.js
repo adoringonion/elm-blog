7896c97e57dcc7575164e09d45aee616c677aedb
adoringonion <adringonion.byazura@gmail.com>
2021-10-03 18:44:50 +0900

    remove disqus

--- public/index.js
+++ public/index.js
@@ -4,13 +4,6 @@ export default {
   load: async function (elmLoaded) {
     const app = await elmLoaded;
     googleAnalytics();
-    disqus("");
-
-    app.ports.loadDisqus.subscribe(function (message) {
-      console.log('loadDisqus', message);
-      disqus(message);
-    });
-
   },
   flags: function () {
     return "You can decode this in Shared.elm using Json.Decode.string!";
@@ -28,19 +21,4 @@ function googleAnalytics() {
   ga('create', 'UA-147049916-2', 'auto');
   ga('send', 'pageview');
 };
-
-function disqus(pageId) {
-
-  var disqus_config = function () {
-    //this.page.url = ;  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = pageId; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-  };
-  
-  (function () { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://adoringonion.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-  })();
-}
 
